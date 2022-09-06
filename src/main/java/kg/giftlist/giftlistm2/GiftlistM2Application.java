bd8020349f8bd85f67f26777c38142edc5618cc0
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-09-06 12:24:08 +0600

    Create empty project

---
+++ src/main/java/kg/giftlist/giftlistm2/GiftlistM2Application.java
@@ -0,0 +1,22 @@
+package kg.giftlist.giftlistm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GiftlistM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftlistM2Application.class, args);
+		System.out.println("Welcome colleagues, project name is Giftlist-M2!");
+	}
+
+	@GetMapping("/")
+	public String greetingPage(){
+		return "<h1>Welcome to Giftlist-M2 Application!!!<h1/>";
+	}
+
+}
---
+++ src/test/java/kg/giftlist/giftlistm2/GiftlistM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.giftlist.giftlistm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftlistM2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
