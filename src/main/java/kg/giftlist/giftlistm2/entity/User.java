85c3e31724f8d9d735173876fd4c0ef17d3693ef
samat <samhardy1927@gmail.com>
2022-09-22 17:58:51 +0300

    uer changes

--- src/main/java/kg/giftlist/giftlistm2/entity/User.java
+++ src/main/java/kg/giftlist/giftlistm2/entity/User.java
@@ -26,6 +26,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements UserDetails {
+
     @Id
     @GeneratedValue(generator = "user_gen", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
