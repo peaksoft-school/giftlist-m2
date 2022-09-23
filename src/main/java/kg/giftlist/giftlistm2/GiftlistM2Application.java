74d9eb8a010a7db032e9f9545bb54cc26f795aee
samat <samhardy1927@gmail.com>
2022-09-23 13:26:29 +0300

    clear code and clear code

--- src/main/java/kg/giftlist/giftlistm2/GiftlistM2Application.java
+++ src/main/java/kg/giftlist/giftlistm2/GiftlistM2Application.java
@@ -2,11 +2,8 @@ package kg.giftlist.giftlistm2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
