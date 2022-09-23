a4cb8cc7e2020a6ad3c99c44f34e82eb7ffeed5a
samat <samhardy1927@gmail.com>
2022-09-23 11:43:41 +0300

    Cross-Origin added

--- src/main/java/kg/giftlist/giftlistm2/controller/AuthController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/AuthController.java
@@ -13,13 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/public")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthController {
 
