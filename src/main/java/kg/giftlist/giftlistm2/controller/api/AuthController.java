fe218ecc9235be6537148c176e1ebb7da589f0a7
Berdibek Kurbanbaev <kurbanbaevberdibek@gmail.com>
2022-09-26 16:44:18 +0300

    first commit

--- src/main/java/kg/giftlist/giftlistm2/controller/api/AuthController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/AuthController.java
@@ -18,8 +18,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/public")
-@CrossOrigin
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -40,6 +38,61 @@ public class AuthController {
         } catch (BadCredentialsException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(loginMapper.loginView("", ValidationType.LOGIN_FAILED, null));
         }
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity processForgotPassword(@RequestParam("email") String email, HttpServletRequest request) {
+        User user = userServiceImpl.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User with email" + email + "not found");
+        }
+        ResetPasswordToken resetToken = new ResetPasswordToken();
+        resetToken.setUser(user);
+        resetToken.setToken(jwtTokenUtil.generateToken(user));
+        resetToken.setExpirationTime(LocalDateTime.now().plusMinutes(30));
+        resetToken = passwordResetTokenServiceImpl.save(resetToken);
+        Mail mail = new Mail();
+        mail.setFrom("arzimatovanurperi@gmail.com");
+        mail.setTo(user.getEmail());
+        mail.setSubject("Password reset request");
+        Map<String, Object> mailModel = new HashMap<>();
+        mailModel.put("token", resetToken);
+        mailModel.put("user", user);
+        String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        mailModel.put("resetUrl",url + "/reset-password?token=" + resetToken.getToken());
+        System.out.println(url + " " + resetToken.getToken());
+        String URL = url + "reset-password?token=" + resetToken.getToken();
+        mail.setModel(mailModel);
+
+        emailServiceImpl.sendEmail(mail,URL);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResetPasswordToken get(@RequestParam String token){
+        ResetPasswordToken resetToken = passwordResetTokenServiceImpl.findByToken(token);
+        if(resetToken == null){
+            System.out.println("token not found");
+            throw  new TokenException("token no found");
+        }else if (resetToken.getExpirationTime().isBefore(LocalDateTime.now())){
+            System.out.println("token is expired");
+            throw new TokenException("token is expired");
+        }else {
+            return resetToken;
+        }
+    }
+
+    @PostMapping("/reset-password")
+    public  User resetPassword(@RequestParam String token,@RequestParam String password,@RequestParam String confirmPassword){
+        ResetPasswordToken resetToken = passwordResetTokenServiceImpl.findByToken(token);
+        User user = resetToken.getUser();
+        if(password.equals(confirmPassword)) {
+            user.setPassword(password);
+        }else {
+            log.error("passwords do not match");
+        }
+        userServiceImpl.updatePassword(user);
+        return user;
     }
 
 }
