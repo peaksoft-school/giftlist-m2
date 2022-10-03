bd9b619a743b98ba414f0c7e1de1c47295d43786
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-03 21:24:13 +0600

    refactor v2

--- src/main/java/kg/giftlist/giftlistm2/db/entity/Holiday.java
+++ src/main/java/kg/giftlist/giftlistm2/db/entity/Holiday.java
@@ -1,5 +1,6 @@
 package kg.giftlist.giftlistm2.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
