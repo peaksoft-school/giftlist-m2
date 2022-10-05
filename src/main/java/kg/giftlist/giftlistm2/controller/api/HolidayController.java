78a22586d8cc18fadff51a3e68416c7fcb706118
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-05 22:02:49 +0600

    refactor v4

--- src/main/java/kg/giftlist/giftlistm2/controller/api/HolidayController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/HolidayController.java
@@ -6,6 +6,7 @@ import kg.giftlist.giftlistm2.controller.payload.HolidayRequest;
 import kg.giftlist.giftlistm2.controller.payload.HolidayResponse;
 import kg.giftlist.giftlistm2.db.service.HolidayService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/holiday")
+@PreAuthorize("hasAuthority('USER')")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "Holiday API", description = "User with role \"User\"  can create, update or delete holidays")
 public class HolidayController {
