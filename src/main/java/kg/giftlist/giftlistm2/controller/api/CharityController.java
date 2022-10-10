ad86571f23b31657320b3f74bf3889a9148a6a7f
samat <samhardy1927@gmail.com>
2022-10-10 18:30:32 +0300

    CharityController changes

--- src/main/java/kg/giftlist/giftlistm2/controller/api/CharityController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/CharityController.java
@@ -12,50 +12,50 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/charities")
+@RequestMapping("api/charities")
 @CrossOrigin
-@Tag(name = "Charity API",description = "User can get charity by id, get all charities, create, update or delete charity")
+@Tag(name = "Charity API", description = "User can get charity by id, get all charities, create, update or delete charity")
 @RequiredArgsConstructor
 public class CharityController {
 
     private final CharityService charityService;
     private final CategoryService categoryService;
 
-    @GetMapping("/{id}")
     @Operation(summary = "Get charity", description = "User can get a charity by id")
+    @GetMapping("{id}")
     public Charity getCharityById(@PathVariable Long id) {
         return charityService.getCharityById(id);
     }
 
-    @GetMapping
     @Operation(summary = "Get charities", description = "User can get all charities")
+    @GetMapping
     public List<Charity> getAllCharities() {
         return charityService.getAllCharities();
     }
 
-    @PostMapping
     @Operation(summary = "Add charity", description = "Authenticated user can create a charity")
+    @PostMapping
     public CharityResponse addCharity(@RequestBody CategoryCharity categoryCharity) {
         categoryService.createCategory(categoryCharity.getCategoryRequest());
         return charityService.createCharity(categoryCharity.getCharityRequest());
     }
 
-    @PostMapping("/{id}")
     @Operation(summary = "charity booking", description = "Authenticated user can book a charity")
+    @PostMapping("{id}")
     public Charity booking(@PathVariable Long id) {
         return charityService.book(id);
     }
 
-    @PutMapping("/{id}")
     @Operation(summary = "Update charity", description = "User can update a charity by id")
+    @PutMapping("{id}")
     public CharityResponse updateCharity(@PathVariable Long id,
                                          @RequestBody CategoryCharity categoryCharity) {
         categoryService.updateCategory(categoryCharity.getCharityRequest().getCategoryId(), categoryCharity.getCategoryRequest());
         return charityService.updateCharity(id, categoryCharity.getCharityRequest());
     }
 
-    @DeleteMapping("/{id}")
     @Operation(summary = "Delete charity", description = "User can delete a charity by id")
+    @DeleteMapping("{id}")
     public String deleteCharity(@PathVariable Long id) {
         return charityService.deleteCharity(id);
     }
