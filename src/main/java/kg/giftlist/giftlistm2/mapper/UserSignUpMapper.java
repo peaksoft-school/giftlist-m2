f7cc55d31f72326d5a84aac88a5ad4b1f1600b68
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-10-20 18:38:02 +0300

    add signup with google2

--- src/main/java/kg/giftlist/giftlistm2/mapper/UserSignUpMapper.java
+++ src/main/java/kg/giftlist/giftlistm2/mapper/UserSignUpMapper.java
@@ -1,7 +1,6 @@
 package kg.giftlist.giftlistm2.mapper;
 
 import kg.giftlist.giftlistm2.controller.payload.SignupRequest;
-import kg.giftlist.giftlistm2.controller.payload.SignupResponse;
 import kg.giftlist.giftlistm2.db.entity.User;
 import kg.giftlist.giftlistm2.db.service.impl.UserService;
 import org.mapstruct.Mapper;
@@ -9,8 +8,8 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring",uses = UserService.class)
 public interface UserSignUpMapper {
+
     @Mapping(target = "password", source = "signupRequest.confirmPassword")
      User toUser(SignupRequest signupRequest);
-    SignupResponse signupResponse(User user);
 
 }
