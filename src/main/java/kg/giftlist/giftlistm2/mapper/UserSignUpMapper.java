d39d5424a7249af10db7bede0024359cacf0d7c1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-28 21:52:31 +0600

    refactor v10

--- src/main/java/kg/giftlist/giftlistm2/mapper/UserSignUpMapper.java
+++
@@ -1,9 +0,0 @@
-package kg.giftlist.giftlistm2.mapper;
-
-import kg.giftlist.giftlistm2.db.service.UserService;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", uses = UserService.class)
-public interface UserSignUpMapper {
-
-}
