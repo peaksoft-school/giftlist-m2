6697479b5587ccf1cb667ff518c14068159c34c1
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-10-29 19:51:48 +0300

    second commit

--- src/main/java/kg/giftlist/giftlistm2/db/service/NotificationService.java
+++ src/main/java/kg/giftlist/giftlistm2/db/service/NotificationService.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
