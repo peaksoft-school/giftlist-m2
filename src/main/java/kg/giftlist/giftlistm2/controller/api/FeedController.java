2c763188bd3b342ffdc9eb5ae2ef63240c5a7af9
lessen12 <baraevba>
2022-11-11 14:02:59 +0600

    add get by id wish

--- src/main/java/kg/giftlist/giftlistm2/controller/api/FeedController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/FeedController.java
@@ -3,13 +3,10 @@ package kg.giftlist.giftlistm2.controller.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.giftlist.giftlistm2.controller.payload.WishListResponse;
-import kg.giftlist.giftlistm2.db.service.FeedService;
+import kg.giftlist.giftlistm2.db.service.WishListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,12 +18,19 @@ import java.util.List;
 @Tag(name = "Feed API", description = "Users with role  \"User\" can see feed")
 public class FeedController {
 
-    private final FeedService feedService;
+    private final WishListService wishListService;
 
     @Operation(summary = "Get friend's wishes and all wishes", description = "User can see friend's wishes and all wishes")
     @GetMapping
     public List<WishListResponse> getAllWishes() {
-        return feedService.getWishesForFeed();
+        return wishListService.getWishesForFeed();
     }
+
+    @Operation(summary = "Get wish list", description = "Getting a wish list by id")
+    @GetMapping("{id}")
+    public WishListResponse getWishListById(@PathVariable Long id) {
+        return wishListService.getWishListById(id);
+    }
+
 
 }
