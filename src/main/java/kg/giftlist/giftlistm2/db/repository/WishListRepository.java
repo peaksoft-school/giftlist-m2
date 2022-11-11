44449980f97077fb52a7db35f91f33b1115a84f3
lessen12 <baraevba>
2022-11-11 13:52:36 +0600

    add isBloc in WishList

--- src/main/java/kg/giftlist/giftlistm2/db/repository/WishListRepository.java
+++ src/main/java/kg/giftlist/giftlistm2/db/repository/WishListRepository.java
@@ -12,10 +12,12 @@ public interface WishListRepository extends JpaRepository<WishList, Long> {
     @Query("select w from WishList w join User u on w.user.id=u.id where u.id=?1")
     List<WishList> getWishListByUserId(Long id);
 
-    @Query("select w from User u join u.friends f join f.wishLists w where u.id=?1 order by w.created DESC")
+    @Query("select w from User u join u.friends f join f.wishLists w where w.isBlock=false and u.id=?1 order by w.created DESC")
     List<WishList> getAllFriendWishes(Long userId);
 
-    @Query("select aw from WishList aw order by aw.created DESC")
+    @Query("select aw from WishList aw where aw.isBlock=false order by aw.created DESC")
     List<WishList> getAllWishes();
+
+
 
 }
