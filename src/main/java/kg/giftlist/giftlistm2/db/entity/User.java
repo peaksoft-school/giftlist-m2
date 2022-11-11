b4cea7b81c2c2fd95110fe86d990520f9f6f1476
lessen12 <baraevba>
2022-11-11 21:23:18 +0600

    commit 4

--- src/main/java/kg/giftlist/giftlistm2/db/entity/User.java
+++ src/main/java/kg/giftlist/giftlistm2/db/entity/User.java
@@ -46,8 +46,6 @@ public class User implements UserDetails {
     @Column(name = "city")
     private String city;
 
-    private String image;
-
     private String password;
 
     @Column(name = "date_of_birth")
@@ -58,8 +56,6 @@ public class User implements UserDetails {
 
     @Size(max = 10000)
     private String hobbies;
-
-    private String city;
 
     @Size(max = 10000)
     @Column(name = "important_to_know")
