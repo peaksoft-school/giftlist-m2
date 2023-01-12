2028ba25c392f25630ff63404d3e2d0d452d8f7b
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-12 16:39:42 +0600

    refactor MailingListController

--- src/main/java/kg/giftlist/giftlistm2/controller/api/MailingListController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/MailingListController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/mailing-list")
-@CrossOrigin
 @RequiredArgsConstructor
-@Tag(name = "Mailing list Api", description = "The Admin can send mailing list to users")
-@SecurityRequirement(name = "Authorization")
+@RequestMapping("api/mailing-list")
 @PreAuthorize("hasAuthority('ADMIN')")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@SecurityRequirement(name = "Authorization")
+@Tag(name = "Mailing list Api", description = "The Admin can send mailing list to users")
 public class MailingListController {
 
     private final MailingListService mailingListService;
