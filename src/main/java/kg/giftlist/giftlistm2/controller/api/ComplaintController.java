673565105730c25b307e22647bcdca0f1b79cea4
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-12 16:28:48 +0600

    refactor ComplaintController

--- src/main/java/kg/giftlist/giftlistm2/controller/api/ComplaintController.java
+++ src/main/java/kg/giftlist/giftlistm2/controller/api/ComplaintController.java
@@ -14,62 +14,56 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/complaints")
-@CrossOrigin
-@Tag(name = "Complaint API", description = "User can complain posts to admin, admin can delete a complain by id, block or unblock the post by id, get posts")
-@SecurityRequirement(name = "Authorization")
 @RequiredArgsConstructor
+@RequestMapping("api/complaints")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@PreAuthorize("hasAuthority('ADMIN')")
+@SecurityRequirement(name = "Authorization")
+@Tag(name = "Complaint API", description = "User can complain posts to admin, admin can delete a complain by id, block or unblock the post by id, get posts")
 public class ComplaintController {
 
     private final ComplaintService complaintService;
 
     @Operation(summary = "Complain the wish list posts to admin", description = "Creating a complaint by wish list id")
-    @PostMapping("wishlist/{id}")
     @PreAuthorize("hasAuthority('USER')")
-    public String createWishlistComplaint(@PathVariable Long id,
-                                          @RequestBody ComplaintRequest request) {
+    @PostMapping("wishlist/{id}")
+    public String createWishlistComplaint(@PathVariable Long id, @RequestBody ComplaintRequest request) {
         return complaintService.createWishlistComplaint(id, request);
     }
 
     @Operation(summary = "Complain the charity posts to admin", description = "Creating a complain by charity id")
-    @PostMapping("charity/{id}")
     @PreAuthorize("hasAuthority('USER')")
-    public String createCharityComplaint(@PathVariable Long id,
-                                         @RequestBody ComplaintRequest request) {
+    @PostMapping("charity/{id}")
+    public String createCharityComplaint(@PathVariable Long id, @RequestBody ComplaintRequest request) {
         return complaintService.createCharityComplaint(id, request);
     }
 
     @Operation(summary = "Get wish list complaint", description = "Admin can get a wish list complaint by id")
     @GetMapping("wishlist/{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public WishlistComplaintResponse getWishlistComplaintById(@PathVariable Long id) {
         return complaintService.getWishlistComplaintById(id);
     }
 
     @Operation(summary = "Get charity complaint", description = "Admin can get a charity complaint by id")
     @GetMapping("charity/{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public CharityComplaintResponse getCharityComplaintById(@PathVariable Long id) {
         return complaintService.getCharityComplaintById(id);
     }
 
     @Operation(summary = "Get all wish list complaints", description = "Admin can get all wish list complaints")
     @GetMapping("wishlist/all")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public List<WishlistComplaintResponse> getAllWishListComplaints() {
         return complaintService.getAllWishListComplaints();
     }
 
     @Operation(summary = "Get all charity complaints", description = "Admin can get all charity complaints")
     @GetMapping("charity/all")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public List<CharityComplaintResponse> getAllCharityComplaints() {
         return complaintService.getAllCharityComplaints();
     }
 
     @Operation(summary = "Delete a complaint", description = "Admin can delete a complaint by id")
     @DeleteMapping("{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteComplaint(@PathVariable Long id) {
         return complaintService.deleteComplaint(id);
     }
