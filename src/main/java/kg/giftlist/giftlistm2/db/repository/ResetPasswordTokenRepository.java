3101ea6447528f870138cd7ae82e6a62beac67b7
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-17 11:53:16 +0600

    refactor ResetPasswordTokenRepository

--- src/main/java/kg/giftlist/giftlistm2/db/repository/ResetPasswordTokenRepository.java
+++ src/main/java/kg/giftlist/giftlistm2/db/repository/ResetPasswordTokenRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ResetPasswordTokenRepository extends JpaRepository<ResetPasswordToken, Long> {
 
-  ResetPasswordToken findByToken(String token);
+    ResetPasswordToken findByToken(String token);
 
 }
