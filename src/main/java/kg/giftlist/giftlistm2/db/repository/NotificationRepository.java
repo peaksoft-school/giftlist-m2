94fe7850e304599798f66ae224f4c9fdb76d898b
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-17 11:52:48 +0600

    refactor NotificationRepository

--- src/main/java/kg/giftlist/giftlistm2/db/repository/NotificationRepository.java
+++ src/main/java/kg/giftlist/giftlistm2/db/repository/NotificationRepository.java
@@ -10,16 +10,16 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query("select n from Notification n join n.receivers r where r.id=?1")
+    @Query("SELECT n FROM Notification n JOIN n.receivers r WHERE r.id=?1")
     List<Notification> getAllNotificationByUserId(Long id);
 
-    @Query("select n from Notification n join n.receivers r where n.receivers=?1")
+    @Query("SELECT n FROM Notification n JOIN n.receivers r WHERE n.receivers=?1")
     Notification getNotificationByUserId(Long id);
 
-    @Query("select n from Notification n join n.receivers r where n.isRead=true and r.id=?1")
+    @Query("SELECT n FROM Notification n JOIN n.receivers r WHERE n.isRead=true AND r.id=?1")
     List<Notification> getAllIsReadNotification(Long id);
 
-    @Query("select n from Notification n join n.receivers r where n.isRead=false and r.id=?1")
+    @Query("SELECT n FROM Notification n JOIN n.receivers r WHERE n.isRead=false AND r.id=?1")
     List<Notification> getAllUnReadNotification(Long id);
 
 }
