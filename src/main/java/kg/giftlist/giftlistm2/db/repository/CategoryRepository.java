3c195fa593b3b3a772bb77a020d74dd3a7ca6538
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-17 11:47:02 +0600

    refactor CategoryRepository v2

--- src/main/java/kg/giftlist/giftlistm2/db/repository/CategoryRepository.java
+++ src/main/java/kg/giftlist/giftlistm2/db/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package kg.giftlist.giftlistm2.db.repository;
 
 import kg.giftlist.giftlistm2.db.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
