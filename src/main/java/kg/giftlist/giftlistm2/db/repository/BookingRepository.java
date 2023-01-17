bb9713c5ace77ff50300d130b19ee592a127171d
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-17 11:22:32 +0600

    refactor BookingRepository

--- src/main/java/kg/giftlist/giftlistm2/db/repository/BookingRepository.java
+++ src/main/java/kg/giftlist/giftlistm2/db/repository/BookingRepository.java
@@ -10,16 +10,10 @@ import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    @Query("select b from Booking b where b.charity is not null and b.userId.id=?1")
+    @Query("SELECT b FROM Booking b WHERE b.charity IS NOT NULL AND b.userId.id=?1")
     List<Booking> getAllCharityBooking(Long userId);
 
-    @Query("select b from Booking b  where b.wishList is not null and  b.userId.id=?1")
+    @Query("SELECT b FROM Booking b WHERE b.wishList IS NOT NULL AND b.userId.id=?1")
     List<Booking> getAllWishListBooking(Long userId);
-
-    @Query("select b from Booking b  where b.wishList is not null")
-    List<Booking> getAllWishListsBooking();
-
-    @Query("select b from Booking b join WishList w on b.wishList.id=w.id where w.id=?1")
-    Booking getWishlist(Long id);
 
 }
