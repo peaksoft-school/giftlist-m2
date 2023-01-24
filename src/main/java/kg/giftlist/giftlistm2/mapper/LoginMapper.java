1a384bf1710d509fdda4884de7b990c6957794bd
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-24 16:38:27 +0600

    refactor LoginMapper

--- src/main/java/kg/giftlist/giftlistm2/mapper/LoginMapper.java
+++ src/main/java/kg/giftlist/giftlistm2/mapper/LoginMapper.java
@@ -5,7 +5,10 @@ import kg.giftlist.giftlistm2.db.entity.User;
 import kg.giftlist.giftlistm2.enums.Role;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class LoginMapper {
